6bf5e709be84f43eb1f3397570a24f6afc64f2d5
Le Viet Vu <levietvu1995@gmail.com>
2025-12-11 15:42:18 +0700

    Update to use Alibaba Qwen model

--- src/services/openAPI.service.ts
+++ src/services/openAPI.service.ts
@@ -7,12 +7,34 @@ const getOpenAIInstance = () => {
     console.log(config.openApiKey)
     openAIInst = new OpenAI({
       apiKey: config.openApiKey,
+      baseURL: "https://dashscope-intl.aliyuncs.com/compatible-mode/v1"
     });
   }
   return openAIInst;
 }
 
+
 export const chatWithGPT = async (prompt: string) => {
+  try {
+    const openAi = getOpenAIInstance();
+    const chatCompletion = await openAi.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: 'user', content: prompt }
+      ],
+      model: 'qwen-plus',
+    });
+
+    console.log('🤖 GPT:', chatCompletion.choices[0].message.content);
+    return chatCompletion.choices[0].message.content || '';
+  } catch (error) {
+    console.log(error);
+  }
+  return 'AI not available';
+}
+
+
+export const chatWithGPTOpenAi = async (prompt: string) => {
   try {
     const openAi = getOpenAIInstance();
     const chatCompletion = await openAi.chat.completions.create({
